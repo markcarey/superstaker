a29864d9757277dc1f815e5347a122a8af6f5ca0
markcarey <mark@markcarey.com>
2022-05-18 15:33:09 -0400

    first cut at contract without calc

--- contracts/SuperStaker.sol
+++ contracts/SuperStaker.sol
@@ -12,10 +12,11 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "./interfaces/aave/ILendingPool.sol";
 import "./interfaces/aave/IDataProvider.sol";
+import "./interfaces/aave/IFlashLoanReceiver.sol";
 import "./interfaces/lido/IStETH.sol";
 import "./interfaces/IWETH9.sol";
 
-contract SuperStaker is Ownable, Pausable, ReentrancyGuard {
+contract SuperStaker is Ownable, Pausable, ReentrancyGuard, IFlashLoanReceiver {
     using SafeMath for uint256;
 
     IStETH public steth;
@@ -45,7 +46,7 @@ contract SuperStaker is Ownable, Pausable, ReentrancyGuard {
         require(msg.value != 0, "ZERO_DEPOSIT");
 
         // @dev stake ETH in Lido
-        uint256 shares = steth.submit{value: msg.value}(referral);
+        //uint256 shares = steth.submit{value: msg.value}(referral);
 
         // @dev get sender Aave stats TODO: remove vars not needed
         (uint256 totalCollateralETH,
@@ -62,8 +63,67 @@ contract SuperStaker is Ownable, Pausable, ReentrancyGuard {
         console.log("ltv", ltv);
         console.log("healthFactor", healthFactor);
 
-        
+
+        uint256 loanAmt = 100;  // TODO: calc flash loan amt
+
+        address[] memory assets = new address[](2);
+        assets[0] = address(weth);
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = loanAmt;
+
+        // 0 = no debt, 1 = stable, 2 = variable
+        uint256[] memory modes = new uint256[](2);
+        modes[0] = 2;
+
+        address onBehalfOf = msg.sender;
+
+        pool.flashLoan(
+            address(this),
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            abi.encode(msg.sender),
+            0
+        );
+
+
 
     }
+
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    )
+        external
+        override
+        returns (bool)
+    {
+        require(msg.sender == address(pool), 'CALLER_MUST_BE_LENDING_POOL');
+        console.log("initiator", initiator);
+        console.log("amount", amounts[0]);
+        console.log("premium", premiums[0]);
+        uint amountOwing = amounts[0].add(premiums[0]);
+
+        (address sender) = abi.decode(params, (address));
+        console.log("sender", sender);
+
+        weth.withdraw(amounts[0]);
+
+        console.log("ETH balance before staking", address(this).balance);
+
+        // @dev stake inital msg.value + flash loan proceeds via Lido
+        uint256 shares = steth.submit{value: address(this).balance}(referral);
+
+        // @dev deposit resulting stETH as collateral in Aave
+        pool.deposit(address(steth), shares, sender, 0);
+        
+        return true;
+    }
+
 
 }
--- contracts/interfaces/aave/ILendingPool.sol
+++ contracts/interfaces/aave/ILendingPool.sol
@@ -12,6 +12,16 @@ interface ILendingPool {
 
     function withdraw(address asset, uint256 amount, address to) external returns (uint256);
 
+    function flashLoan(
+        address receiverAddress,
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata modes,
+        address onBehalfOf,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+
     function getUserAccountData(address user) external view returns (
         uint256 totalCollateralETH,
         uint256 totalDebtETH,
