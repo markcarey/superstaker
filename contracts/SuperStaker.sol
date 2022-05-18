ce2b300dbee0eed9e32fefcbf9ac5a80e1b520fc
markcarey <mark@markcarey.com>
2022-05-18 10:04:40 -0400

    first part of stake function

--- contracts/SuperStaker.sol
+++ contracts/SuperStaker.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >= 0.8.0;
+pragma solidity ^0.8.1;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,10 +8,62 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 import "./interfaces/aave/ILendingPool.sol";
 import "./interfaces/aave/IDataProvider.sol";
 import "./interfaces/lido/IStETH.sol";
+import "./interfaces/IWETH9.sol";
 
-contract SuperStaker is Ownable {
+contract SuperStaker is Ownable, Pausable, ReentrancyGuard {
+    using SafeMath for uint256;
+
+    IStETH public steth;
+    IWETH9 public weth;
+    address public referral;
+
+    // Aave:
+    address public aToken;
+    address public varDebtToken;
+    ILendingPool public pool;
+
+    constructor(address payable _weth, address _steth, address _dataProvider, address _pool, address _referral) {
+        steth = IStETH(_steth);
+        weth = IWETH9(_weth);
+        referral = _referral;
+
+        pool = ILendingPool(_pool);
+        (aToken,,) = IDataProvider(_dataProvider).getReserveTokensAddresses(_steth);
+        (,,varDebtToken) = IDataProvider(_dataProvider).getReserveTokensAddresses(_weth);
+
+        steth.approve(_pool, 2**256 - 1);
+    }
+
+    // @dev SuperStakes native ETH sent to the function
+    function stake() payable external nonReentrant {
+        address sender = msg.sender;
+        require(msg.value != 0, "ZERO_DEPOSIT");
+
+        // @dev stake ETH in Lido
+        uint256 shares = steth.submit{value: msg.value}(referral);
+
+        // @dev get sender Aave stats TODO: remove vars not needed
+        (uint256 totalCollateralETH,
+        uint256 totalDebtETH,
+        uint256 availableBorrowsETH,
+        uint256 currentLiquidationThreshold,
+        uint256 ltv,
+        uint256 healthFactor) = pool.getUserAccountData(sender);
+
+        console.log("totalCollateralETH", totalCollateralETH);
+        console.log("totalDebtETH", totalDebtETH);
+        console.log("availableBorrowsETH", availableBorrowsETH);
+        console.log("currentLiquidationThreshold", currentLiquidationThreshold);
+        console.log("ltv", ltv);
+        console.log("healthFactor", healthFactor);
+
+        
+
+    }
 
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,48 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-waffle");
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
-  solidity: "0.7.3",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100
+          }
+        }
+      }
+    ] 
+},
+   defaultNetwork: "hardhat",
+   networks: {
+    hardhat: {
+      accounts: [{ privateKey: `0x${PRIVATE_KEY}`, balance: "10000000000000000000000"}],
+      forking: {
+        url: API_URL,
+        blockNumber: 14799179  // assumes ETH mainnet fork
+      },
+      loggingEnabled: true,
+      gasMultiplier: 7,
+      gasPrice: 1000000000 * 5,
+      blockGasLimit: 0x1fffffffffffff
+    },
+    ethereum: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    }
+   },
+   etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+}
+
+// 
+// npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/y2J0jJRJ0W0l7e7J1CVv4zRj-GgBjNHP --fork-block-number 14799179
